36a02bb385f1ffab41db9dbc67ab8619e146a2d2
Artem Komarov <95048367@mail.schwarz>
2024-12-25 15:14:08 +0100

    O(n) solution for Container With Most Water

--- ContainerWithMostWater.py
+++ ContainerWithMostWater.py
@@ -2,125 +2,32 @@ import sys
 from typing import List
 
 class Solution:
-    def calculateBest(self, height: List[int], leftBoundary: int, rightBoundary: int, newLeftBoundary: int, newRightBoundary: int):
-        bestLeft = leftBoundary
-        bestRight = rightBoundary
-        maxArea = self.calculateArea(height, leftBoundary, rightBoundary)
-
-        for l, r in [[leftBoundary, newRightBoundary], [newLeftBoundary, rightBoundary], [newLeftBoundary, newRightBoundary], [leftBoundary, newLeftBoundary], [newRightBoundary, rightBoundary]]:
-            area = self.calculateArea(height, l, r)
-            if area > maxArea:
-                maxArea = area
-                bestLeft = l
-                bestRight = r
-
-        return maxArea, bestLeft, bestRight
-
-
-    def maxArea(self, height: List[int]) -> int:
-        leftBoundary = 0
-        rightBoundary = len(height) - 1
-        maxArea = self.calculateArea(height, leftBoundary, rightBoundary)
-        
-        newLeftBoundary = 1
-        newRightBoundary = rightBoundary - 1
-
-        while newLeftBoundary < newRightBoundary:
-            if height[newLeftBoundary] <= height[leftBoundary]:
-                area = self.calculateArea(height, leftBoundary, newLeftBoundary)
-                if area > maxArea:
-                    maxArea = area
-                newLeftBoundary += 1
-                continue
-
-            if height[newRightBoundary] <= height[rightBoundary]:
-                area = self.calculateArea(height, newRightBoundary, rightBoundary)
-                if area > maxArea:
-                    maxArea = area
-                newRightBoundary -= 1
-                continue
-            
-
-
-            area, bestLeftBoundary, bestRightBoundary = self.calculateBest(height, leftBoundary, rightBoundary, newLeftBoundary, newRightBoundary)
-            if area > maxArea:
-                maxArea = area
-
-            if bestLeftBoundary == leftBoundary:
-                if bestRightBoundary == newRightBoundary:
-                    rightBoundary = newRightBoundary
-                    newRightBoundary += 1
-                elif bestRightBoundary == newLeftBoundary:
-                    rightBoundary = newRightBoundary
-                    newRightBoundary -= 1
-                else: # bestRightBoundary == rightBoundary
-                    if newRightBoundary > newLeftBoundary:
-                        newLeftBoundary += 1
-                    elif newRightBoundary < newLeftBoundary:
-                        newRightBoundary -= 1
-                    else:
-                        newLeftBoundary += 1
-                        newRightBoundary -= 1
-            elif bestLeftBoundary == newLeftBoundary:
-                leftBoundary = newLeftBoundary
-
-                if bestRightBoundary == newRightBoundary:
-                    rightBoundary = newRightBoundary
-                else:
-                    rightBoundary -= 1
-            else: # bestLeftBoundary == newRightBoundary
-                leftBoundary = newLeftBoundary
-                newLeftBoundary += 1
-
-        # Last check
-        area, _, _, = self.calculateBest(height, leftBoundary, rightBoundary, newLeftBoundary, newRightBoundary)
-        if area > maxArea:
-            maxArea = area
-
-        return maxArea
+    def calculateArea(self, height: List[int], left_boundary: int, right_boundary: int) -> int:
+        return (right_boundary - left_boundary) * min(height[left_boundary], height[right_boundary])
     
-    def calculateArea(self, height: List[int], leftBoundary: int, rightBoundary: int) -> int:
-        return (rightBoundary - leftBoundary) * min(height[leftBoundary], height[rightBoundary])
+    def maxArea(self, height: List[int]) -> int:
+        left_boundary = 0
+        right_boundary = len(height) - 1
+        maxArea = 0
+        while left_boundary < right_boundary:
+            area = self.calculateArea(height, left_boundary, right_boundary)
+            if area > maxArea:
+                maxArea = area
 
-    def maxAreaS2(self, height: List[int]) -> int:
-        maxArea = self.calculateArea(height, 0, len(height)-1)
-
-        leftBiggest = 0
-        leftBiggestHeight = height[0]
-
-        for i1 in range(0, len(height) - 1):
-            if height[i1] < leftBiggestHeight:
-                area = self.calculateArea(height, leftBiggest, i1)
-                if area > maxArea:
-                    maxArea = area
-                continue
+            if height[left_boundary] > height[right_boundary]:
+                right_boundary -= 1
+            elif height[left_boundary] < height[right_boundary]:
+                left_boundary += 1
             else:
-                leftBiggest = i1
-                leftBiggestHeight = height[i1]
-
-            rightBiggestHeight = height[len(height)-1]
-            rightBiggest = len(height) - 1
-            for i2 in reversed(range(i1 + 1, len(height))):
-                if height[i2] < rightBiggestHeight:
-                    area = self.calculateArea(height, i2, rightBiggest)
-                    if area > maxArea:
-                        maxArea = area
-                    continue
-                else:
-                    rightBiggestHeight = height[i2]
-                    rightBiggest = i2
-
-                area = min(height[i1], height[i2]) * (i2 - i1)
-                if area > maxArea:
-                    maxArea = area
+                left_boundary += 1
+                right_boundary -= 1
 
         return maxArea
-
 
 
 def printCase(heights, sol):
     solution = Solution()
-    print(solution.maxAreaS2(heights), sol)
+    print(solution.maxArea(heights), sol)
 
 def main() -> int:
     printCase([10,14,10,4,10,2,6,1,6,12], 96)
